d4fffa5fdbaceab58f34b4a0d4bb1c858abbf600
adkow <a.kowalewski.student@gmail.com>
2020-08-29 14:59:26 +0200

    GenericDao interface.

--- src/main/java/com/sda/jpa/dao/GenericDao.java
+++ src/main/java/com/sda/jpa/dao/GenericDao.java
@@ -1,4 +1,16 @@
 package com.sda.jpa.dao;
 
+import java.util.List;
+
 public interface GenericDao <T> {
+
+    T get (int id);
+
+    void delete (int id);
+
+    List<T> getAll();
+
+    void update (T entity);
+    
+    void save (T entity);
 }
